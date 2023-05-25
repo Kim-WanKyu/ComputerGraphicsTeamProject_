c61661733b155828d6b39f050d0a461b4f4c9f63
Kim-WanKyu <qwertnmagicalmaze@gmail.com>
2023-05-25 15:29:53 +0900

    총알과 총알스포너 및 땅 떨어지는거, 점프한번만되는거 수정
    Bullet프리팹 isTrigger 켜줌
    Bullet스크립트 수정 (발사방향이 캐릭터 방향으로) - 근데 캐릭터 다리방향으로 발사됨. 나중에 찾아보고 수정할 예정
    BulletSpawner스크립트 수정. (스폰 x축 랜덤으로 지정해봄. 근데 필요없음.)
    Player스크립트 수정 (Floor태그->Ground태그로 수정) - 바닥 태그
    Plane오브젝트의 리지드바디 삭제 박스 콜라이더 추가
    BulletSpawner오브젝트 콜라이더 삭제

--- UniversitySurvivalGame/Assets/Scripts/BulletSpawner.cs
+++ UniversitySurvivalGame/Assets/Scripts/BulletSpawner.cs
@@ -8,6 +8,9 @@ public class BulletSpawner : MonoBehaviour
     public float spawnRateMin = 0.5f;
     public float spawnRateMax = 3f;
 
+    //생성 범위 추가
+    public Vector3 spawnPosition;
+
     private Transform target;
     private float spawnRate;
     private float timeAfterSpawn;
@@ -17,6 +20,7 @@ public class BulletSpawner : MonoBehaviour
         this.timeAfterSpawn = 0f;
         this.spawnRate = Random.Range(spawnRateMin, spawnRateMax);
         this.target = FindObjectOfType<Player>().transform;
+        spawnPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -24,13 +28,17 @@ public class BulletSpawner : MonoBehaviour
     {
 
         timeAfterSpawn += Time.deltaTime;
+        spawnPosition = new Vector3(
+            Random.Range(transform.position.x - 10f, transform.position.x + 10f),
+            transform.position.y,
+            transform.position.z );
 
         if (this.timeAfterSpawn >= this.spawnRate)
         {
             timeAfterSpawn = 0f;
 
             GameObject bullet
-                = Instantiate(this.bulletPrefab, this.transform.position, this.transform.rotation);
+                = Instantiate(this.bulletPrefab, spawnPosition, this.transform.rotation);
 
             bullet.transform.LookAt(target);
 
